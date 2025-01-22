140d6581990ee9d95d228ab255bcf0be679545b9
sedomu <seguin@lilo.org>
2025-01-22 08:49:29 +0100

    Ajouter confirmation quand envoi réussi #4 - completed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,10 +7,24 @@ function editNav() {
   }
 }
 
+// form fields initialisation
+function resetForm(){
+  firstName.value = "";
+  lastName.value = "";
+  email.value = "";
+  birthDate.value = "";
+  tournamentsNumber.value = "";
+  for (let i = 0; i < cityChoice.length; i++){
+    cityChoice[i].checked = false;
+  }
+  terms.checked = true;
+  news.checked = false;
+}
+
 // verify regular expressions
 function exprCheck(element, expr) {
-  if (element.value === "") {
-    element.parentElement.setAttribute("data-error-visible", "false");
+  if (element.value === "" || !expr.test(element.value)) {
+    element.parentElement.setAttribute("data-error-visible", "true");
     element.parentElement.removeAttribute("data-error");
     return 0;
   } else if (expr.test(element.value)) {
@@ -23,9 +37,104 @@ function exprCheck(element, expr) {
   }
 }
 
+// names verification function
+function nameCheck(name, position) {
+  let score = exprCheck(name, exprName);
+
+  if (score === 0){
+    switch (position) {
+      case "first":
+        firstName.parentElement.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      case "last":
+        lastName.parentElement.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    }
+  }
+
+  return score;
+}
+
+// email verification function
+function emailCheck(email) {
+    let score = exprCheck(email, exprEmail);
+
+    if (score === 0) {
+      email.parentElement.setAttribute("data-error", "Veuillez renseigner une adresse email correcte pseudo@domain.tld");
+    }
+
+    return score;
+}
+
+// date verification function
+function dateCheck(date){
+    let score = 0;
+
+    if (isNaN(Date.parse(date.value))) {
+      score = 0;
+      date.parentElement.setAttribute("data-error-visible", "true");
+      date.parentElement.setAttribute("data-error", "Veuillez renseigner une date correcte jj/mm/aaaa");
+    } else {
+      score = 1;
+      date.parentElement.setAttribute("data-error-visible", "false");
+      date.parentElement.removeAttribute("data-error");
+    }
+
+    return score;
+}
+
+// quantity verification function
+function quantityCheck(quantity){
+    let score = exprCheck(quantity, exprQuantity);
+
+    if (score === 0) {
+      tournamentsNumber.parentElement.setAttribute("data-error", "Veuillez renseigner le nombre de vos anciennes participations (>=0).");
+    }
+
+    return score;
+}
+
+// radio buttons check function
+function radioCheck(radio){
+  let score = 0;
+
+  for (let i = 0; i < radio.length; i++) {
+      if (radio[i].checked){
+        score = 1;
+        break;
+      }
+    }
+
+  if (score === 0) {
+    radio[0].parentElement.setAttribute("data-error-visible", "true");
+    radio[0].parentElement.setAttribute("data-error", "Vous devez choisir une option.");
+  } else {
+    radio[0].parentElement.setAttribute("data-error-visible", "false");
+    radio[0].parentElement.removeAttribute("data-error");
+  }
+
+  return score;
+}
+
+// checkbox "checked" function
+function checkboxCheck(checkbox){
+  let score = 0;
+
+  if (checkbox.checked) {
+    checkbox.parentElement.setAttribute("data-error-visible", "false");
+    checkbox.parentElement.removeAttribute("data-error");
+    score = 1;
+  } else {
+    checkbox.parentElement.setAttribute("data-error-visible", "true");
+    checkbox.parentElement.setAttribute("data-error", "Les conditions d'utilisation doivent être acceptées");
+    score = 0;
+  }
+
+  return score;
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalValid = document.querySelector(".modal-validation");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector("span.close");
 const firstName = document.getElementById("first");
@@ -33,10 +142,12 @@ const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const tournamentsNumber = document.getElementById("quantity");
-const terms = document.getElementById("checkbox1");
 const cityChoice = document.querySelectorAll('input[name="location"]');
+const terms = document.getElementById("checkbox1");
+const news = document.getElementById("checkbox2");
 const submitBtn = document.querySelector(".btn-submit");
 const allInputs = document.querySelectorAll(".formData > input");
+const validBtn = document.querySelector(".btn-validation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -55,6 +166,9 @@ function closeModal(){
 }
 
 // form validations
+// form fields initialisation
+resetForm();
+
 // validation counters
 let firstValid = 0;
 let lastValid = 0;
@@ -77,89 +191,36 @@ const exprQuantity = new RegExp("^\\d{1,2}$");
 submitBtn.disabled = true;
 
 // first name
-firstName.addEventListener("change", () => {
-  firstValid = exprCheck(firstName, exprName);
-  if (firstValid === 0) {
-    firstName.parentElement.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-  }
-});
+firstName.addEventListener("change", () => {firstValid = nameCheck(firstName, "first");});
 
 // last name
-lastName.addEventListener("change", () => {
-  lastValid = exprCheck(lastName, exprName);
-  if (lastValid === 0) {
-    lastName.parentElement.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-  }
-});
+lastName.addEventListener("change", () => {lastValid = nameCheck(lastName, "last");});
 
 // email
-email.addEventListener("change", () => {
-  emailValid = exprCheck(email, exprEmail);
-  if (emailValid === 0) {
-    email.parentElement.setAttribute("data-error", "Veuillez renseigner une adresse email correcte pseudo@domain.tld");
-  }
-});
+email.addEventListener("change", () => {emailValid = emailCheck(email);});
 
 // birthdate
-birthDate.addEventListener("blur", () => {
-  if (isNaN(Date.parse(birthDate.value))) {
-    dateValid = 0;
-    birthDate.parentElement.setAttribute("data-error-visible", "true");
-    birthDate.parentElement.setAttribute("data-error", "Veuillez renseigner une date correcte jj/mm/aaaa");
-  } else {
-    dateValid = 1;
-    birthDate.parentElement.setAttribute("data-error-visible", "false");
-    birthDate.parentElement.removeAttribute("data-error");
-  }
-});
+birthDate.addEventListener("blur", () => {dateValid = dateCheck(birthDate);});
 
 // quantity of past participations
 tournamentsNumber.addEventListener("change", () => {
-  quantityValid = exprCheck(tournamentsNumber, exprQuantity);
-  if (quantityValid === 0) {
-    tournamentsNumber.parentElement.setAttribute("data-error", "Veuillez renseigner le nombre de vos anciennes participations (>=0).");
-  }
+  quantityValid = quantityCheck(tournamentsNumber);
 });
 
-
 // tournament choice
-// radio buttons: initialising error message in the first place
-cityChoice[0].parentElement.setAttribute("data-error-visible", "true");
-cityChoice[0].parentElement.setAttribute("data-error", "Vous devez choisir une option.");
-
-// validation
 for (let i = 0; i < cityChoice.length; i++) {
-  cityChoice[i].addEventListener("change", () => {
-    if (cityChoice[i].checked){
-      locationValid = 1;
-    }
-    if (locationValid === 0) {
-      cityChoice[0].parentElement.setAttribute("data-error-visible", "true");
-      cityChoice[0].parentElement.setAttribute("data-error", "Vous devez choisir une option.");
-    } else {
-      cityChoice[0].parentElement.setAttribute("data-error-visible", "false");
-      cityChoice[0].parentElement.removeAttribute("data-error");
-    }
-  })
+  cityChoice[i].addEventListener("change", () => {locationValid = radioCheck(cityChoice);})
 }
 
 // terms and conditions accepted
 terms.addEventListener("change", () => {
-  if (terms.checked) {
-    terms.parentElement.setAttribute("data-error-visible", "false");
-    terms.parentElement.removeAttribute("data-error");
-    termsValid = 1;
-  } else {
-    terms.parentElement.setAttribute("data-error-visible", "true");
-    terms.parentElement.setAttribute("data-error", "Les conditions d'utilisation doivent être acceptées");
-    termsValid = 0;
-  }
+  termsValid = checkboxCheck(terms);
 })
 
 // enabling submit
 for (let i = 0; i < allInputs.length; i++){
   allInputs[i].addEventListener("change", () => {
-    console.log("coucou");
+    
     const scoreValid = firstValid + lastValid + emailValid + dateValid + quantityValid + locationValid + termsValid;
     if (scoreValid === 7) {
       submitBtn.disabled = false;
@@ -170,4 +231,45 @@ for (let i = 0; i < allInputs.length; i++){
 }
 
 // submit button behaviour
-submitBtn.addEventListener("click", (e) => {e.preventDefault();});
+// running each verification to help user to identify missing fields
+// in terms of UX: really bad... ugly...
+// submitBtn.addEventListener("mouseover", () => {
+//   firstValid = nameCheck(firstName, "first");
+//   lastValid = nameCheck(lastName, "last");
+//   emailValid = emailCheck(email);
+//   dateValid = dateCheck(birthDate);
+//   quantityValid = quantityCheck(tournamentsNumber);
+//   locationValid = radioCheck(cityChoice);
+//   termsValid = checkboxCheck(terms);
+// })
+
+submitBtn.addEventListener("click", (e) => {
+  // prevent page from reloading
+  e.preventDefault();
+
+  // final data validation check-up to prevent code injection
+  // and validation or error messages
+  let submitScore = 0;
+  
+  submitScore += nameCheck(firstName, "first");
+  submitScore += nameCheck(lastName, "last");
+  submitScore += emailCheck(email);
+  submitScore += dateCheck(birthDate);
+  submitScore += quantityCheck(tournamentsNumber);
+  submitScore += radioCheck(cityChoice);
+  submitScore += checkboxCheck(terms);
+
+  if (submitScore === 7) {
+    modalValid.style.display = "flex";
+  } else {
+    // warning messages are displayed using ...Check functions
+    submitBtn.disabled = true;
+  }
+});
+
+// closing and resetting modal after validation
+validBtn.addEventListener("click", () => {
+  modalValid.style.display = "none";
+  resetForm();
+  closeModal();
+})
